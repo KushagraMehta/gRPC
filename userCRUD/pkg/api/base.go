7e5bfa3421a9451eb89ee1e0cd764ad483689cdf
Kushagra Mehta <kushagramehta2000@gmail.com>
2021-04-01 19:56:23 +0530

    Remving infinite loop ⛔

--- userCRUD/pkg/api/base.go
+++ userCRUD/pkg/api/base.go
@@ -25,19 +25,21 @@ func (server *Server) Initialize() {
 	var err error
 	var DBURL string
 	if os.Getenv("LOCAL") == "1" {
-		DBURL = fmt.Sprintf("postgres://%v:%v@%v:5432/crud", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), "localhost")
+		DBURL = fmt.Sprintf("postgres://%v:%v@%v:5432/crud", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), "localhostf")
 	} else {
 		DBURL = fmt.Sprintf("postgres://%v:%v@%v:5432/crud", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("HOST"))
 	}
-	for i := 0; i < 5; i++ {
+	for i := 1; i < 10; i++ {
 		if server.DB, err = pgxpool.Connect(context.Background(), DBURL); err != nil {
 			log.Println("Cannot connect to database")
-			log.Fatal("This is the error:", err)
-			log.Printf("%v: Trying to connect to Database after ", i)
+			log.Printf("%v:Trying to connect to Database after 0.5Sec", i)
 			time.Sleep(500 * time.Millisecond)
 		} else {
 			log.Println("We are connected to the database")
 			break
+		}
+		if i == 4 {
+			log.Fatal("This is the error:", err)
 		}
 	}
 }
